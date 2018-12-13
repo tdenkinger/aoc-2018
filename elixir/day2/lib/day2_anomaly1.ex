f80f9a9b4acb5fbeae3a48f336efe641504d49c7
Troy Denkinger <tdenkinger@theorchard.com>
2018-12-13 16:04:22 -0500

    Correctly calculate the checksum of all the repeat character occurrences.

--- elixir/day2/lib/day2_anomaly1.ex
+++ elixir/day2/lib/day2_anomaly1.ex
@@ -1,7 +1,46 @@
 defmodule Day2Anomaly1 do
-  defp filter_occurrences(occurrences) do
+  def run(file) do
+    File.stream!(file)
+    |> Stream.map(&String.trim_trailing/1)
+    |> calculate_checksum
+  end
+
+  def calculate_checksum(codes) do
+    codes
+    |> Enum.reduce(%{twos: 0, threes: 0}, fn code, acc ->
+      occurrences = get_unique_occurrences(code)
+      update_counts(acc, occurrences)
+    end)
+    |> final_calculation
+  end
+
+  defp final_calculation(final_counts) do
+	final_counts[:twos] * final_counts[:threes]
+  end
+
+  defp update_counts(acc, [2, 3]) do
+    acc = Map.update!(acc, :twos, &(&1 + 1))
+    Map.update!(acc, :threes, &(&1 + 1))
+  end
+
+  defp update_counts(acc, [2]), do: Map.update!(acc, :twos, &(&1 + 1))
+  defp update_counts(acc, [3]), do: Map.update!(acc, :threes, &(&1 + 1))
+  defp update_counts(acc, _), do: acc
+
+  defp get_unique_occurrences(code) do
+    code
+    |> tokenize
+    |> count_occurrences
+    |> filter_unique_occurrences
+  end
+
+  defp filter_unique_occurrences(occurrences) do
     occurrences
     |> Enum.filter(fn {_k, v} -> v == 2 || v == 3 end)
+    |> Enum.into(%{})
+    |> Map.values
+    |> Enum.uniq
+    |> Enum.sort
   end
 
   defp count_occurrences(tokens) do
@@ -9,11 +48,10 @@ defmodule Day2Anomaly1 do
     |> Enum.reduce(%{}, fn token, counts ->
       cond do
         Map.has_key?(counts, token) -> Map.update!(counts, token, &(&1 + 1))
-        true                        -> Map.put(counts, token, 1)
+        true -> Map.put(counts, token, 1)
       end
     end)
   end
 
   defp tokenize(word), do: String.graphemes(word)
-
 end
--- elixir/day2/test/day2_anomaly1_test.exs
+++ elixir/day2/test/day2_anomaly1_test.exs
@@ -1,23 +1,18 @@
 defmodule Day2Anomaly1Test do
   use ExUnit.Case
 
-  test "does stuff" do
-    target_tokens = [{"b", 3}, {"c", 2}]
+  test "calculates the checksum" do
+    test_data = ~w(abcdef
+                   bababc
+                   abbcde
+                   abcccd
+                   aabcdd
+                   abcdee
+                   ababab)
+
+    checksum = Day2Anomaly1.calculate_checksum(test_data)
+
+    assert checksum == 12
   end
-
-  # test "finds counts equal to 2" do
-  #   occurrences = %{"a" => 1, "b" => 3, "c" => 2}
-
-  #   twos = Day2Anomaly1.filter_occurrences(occurrences)
-
-  #   assert twos == [{"b", 3}, {"c", 2}]
-  # end
-
-  # test "counts occurrences of tokens" do
-  #   tokens = ["a", "b", "c", "c"]
-
-  #   occurrences = Day2Anomaly1.count_occurrences(tokens)
-
-  #   assert occurrences == %{"a" => 1, "b" => 1, "c" => 2}
-  # end
 end
+
